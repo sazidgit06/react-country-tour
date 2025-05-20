d8f20d621298bf6efd0ee911f1e716e119f799c8
Sazid <sazidtec06@gmail.com>
2025-05-21 00:18:40 +0600

    images grid done

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([])
@@ -12,9 +13,11 @@ const Countries = () => {
     return (
         <div>
             <h4>Countries: {countries.length}</h4>
-            {
-                countries.map(country => <Country country={country}></Country>)
-            }
+            <div className="grid">
+                {
+                    countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,9 +1,11 @@
+import './Country.css'
 const Country = ({country}) => {
-    const {name} = country;
+    const {name, flags} = country;
     console.log(country)
     return (
-        <div>
-            <h5>Name: {name.common}</h5>
+        <div className="box">
+            <h5>Name: {name?.common}</h5>
+            <img src={flags.png} alt="" />
         </div>
     );
 };
