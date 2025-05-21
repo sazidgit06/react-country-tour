5b7e32234f75d2914436f01379835f1f703f75ac
Sazid <sazidtec06@gmail.com>
2025-05-21 14:54:27 +0600

    region and population added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,14 @@
 import './Country.css'
 const Country = ({country}) => {
-    const {name, flags} = country;
+    const {name, flags, population, timezones, region} = country;
     console.log(country)
     return (
         <div className="box">
             <h5>Name: {name?.common}</h5>
             <img src={flags.png} alt="" />
+            <h5>Population: {population}</h5>
+            <h5>Timezone: {timezones}</h5>
+            <h5>Region: {region}</h5>
         </div>
     );
 };
